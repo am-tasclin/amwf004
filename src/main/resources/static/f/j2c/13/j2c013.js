c86282126dd43bee93d0a9e89c2d81733629e190
romanm <roman.mishchenko@gmail.com>
2020-06-21 22:44:12 +0300

    c_context_menu

--- src/main/resources/static/f/j2c/13/j2c013.js
+++ src/main/resources/static/f/j2c/13/j2c013.js
@@ -1,6 +1,10 @@
 var initJ2C = function(){
 	console.log('read -> ',371831)
 	read_element_descendant(371831)
+	ctrl.conf_menu_click = function(e) {
+		console.log(e)
+		e.open_children = !e.open_children
+	}
 	ctrl.mu = {}
 	ctrl.mu_mOpenKey = 'mdn'
 	ctrl.mu.mdn = 'Метадані'
