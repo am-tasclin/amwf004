41d5daf3ace879179bc5bafc6ecd969d83f41aae
romanm <roman.mishchenko@gmail.com>
2020-06-23 23:36:49 +0300

    add thymeleaf

---
+++ src/main/java/org/algoritmed/amwf004/tlc/RestTl001.java
@@ -0,0 +1,15 @@
+package org.algoritmed.amwf004.tlc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class RestTl001 {
+	@GetMapping("/v/f1")
+	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+		model.addAttribute("name", name);
+		return "v/f1";
+	}
+}
