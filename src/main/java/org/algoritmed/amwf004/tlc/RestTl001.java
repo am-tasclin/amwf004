1245b301a0b643380536953af57e3fffa560556f
romanm <roman.mishchenko@gmail.com>
2020-06-27 13:00:17 +0300

    x

--- src/main/java/org/algoritmed/amwf004/tlc/RestTl001.java
+++ src/main/java/org/algoritmed/amwf004/tlc/RestTl001.java
@@ -1,17 +1,24 @@
 package org.algoritmed.amwf004.tlc;
 
+import java.util.HashMap;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.reactive.function.client.WebClient;
 
 @Controller
 public class RestTl001 {
+	@GetMapping("/v/f2")
+	public String f2(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+		model.addAttribute("name", name);
+		return "v/f2";
+	}
 	@GetMapping("/v/f1")
 	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
 		model.addAttribute("name", name);
-		WebClient client1 = WebClient.create();
+		HashMap<Object, Object> hashMap = new HashMap<>();
+		
 		return "v/f1";
 	}
 }
