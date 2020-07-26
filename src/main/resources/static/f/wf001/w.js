65f3631e942b54c627ef76bb701d7086ada0b864
romanm <roman.mishchenko@gmail.com>
2020-07-26 12:04:50 +0300

    work flow fitst invoice

--- src/main/resources/static/f/wf001/w.js
+++ src/main/resources/static/f/wf001/w.js
@@ -2,5 +2,6 @@ app.controller('AppCtrl', function($scope, $http, $timeout) {
 	ctrl = this
 	initApp($scope, $http, $timeout)
 	ctrl.page_title = 'wf_ed001: '
+	read2 = new Read2($http)
 	initWF001()
 })
--- src/main/resources/static/f/wf001/wf001.js
+++ src/main/resources/static/f/wf001/wf001.js
@@ -1,5 +1,6 @@
 var initWF001 = function(){
 	console.log('read -> ',111)
+
 	if(ctrl.request.parameters.p){
 		console.log('read -> ', ctrl.request.parameters.p)
 		read_element_descendant(ctrl.request.parameters.p
@@ -21,5 +22,91 @@ var initWF001 = function(){
 			})
 			}
 		})
+	}
+
+	// var  read_invoices_sql = 371989
+	// read2_element(read_invoices_sql,{fn:function(response){
+	// 	read2_element_children(read_invoices_sql, {fnAfter:function(response){
+	// 		var e = ctrl.eMap[read_invoices_sql]
+	// 		// console.log(e, e.sql)
+	// 	}})
+	// }})
+}
+
+
+var read2_element = function (doc_id, fnObj) {
+	var o = ctrl.eMap[doc_id]
+	if (!o) {
+		var sql = sql_app.SELECT_obj_with_i18n(doc_id)
+		read2.http.get(read2.url
+			, { params: { sql: sql } })
+			.then(function (response) {
+				var d = response.data.list[0]
+				// console.log(d, doc_id)
+				ctrl.eMap[d.doc_id] = d
+				if (fnObj && fnObj.fn) (
+					fnObj.fn(response)
+				)
+			})
+	}
+}
+
+var read2_element_children = function (doc_id, fnObj) {
+	var o = ctrl.eMap[doc_id]
+	if (o && o.cnt_child > 0 && (!o.children || o.children.length < o.cnt_child)) {
+		var sql = sql_app.SELECT_children_with_i18n(doc_id)
+		read2.http.get(read2.url
+			, { params: { sql: sql } })
+			.then(function (response) {
+				angular.forEach(response.data.list, function (d) {
+					ctrl.eMap[d.doc_id] = d
+					var parent = ctrl.eMap[d.parent]
+					if (parent) {
+						if (!parent.children) {
+							parent.children = []
+						}
+						parent.children.push(d)
+					}
+
+				})
+				if (o.reference == 371968) {/** Element */
+					if (o.reference2 == 371985) {/** SQL.SELECT */
+						buildElementSelect(o)
+					}
+				}
+				if (fnObj && fnObj.fnAfter) (
+					fnObj.fnAfter(response)
+				)
+			})
+
+	}
+}
+
+var buildElementSelect = function (el) {
+	var addWhere
+	el.sql = 'SELECT * FROM doc d \n'
+	if(el.doctype == 25){/**value.timestamp */
+		el.sql += 'LEFT JOIN timestamp ts ON ts.timestamp_id=d.doc_id \n'
+	}
+	angular.forEach(el.children, function (elC) {
+		if (elC.reference == 371992) {/**SELECT.WHERE.parent */
+			addWhere = 'parent = ' + elC.reference2
+		}
+	})
+	el.sql += ' WHERE '+addWhere
+	// console.log(el, el.sql)
+}
+
+var read2
+class Read2 {
+	constructor($http) {
+		this.http = $http
+		this.url = '/r/url_sql_read_db1'
+		this.sql1 = function(a){
+			read2.http.get(read2.url, {params:a.params})
+			.then(function(response){
+				a.fn(response)
+			})
+		}
 	}
 }
