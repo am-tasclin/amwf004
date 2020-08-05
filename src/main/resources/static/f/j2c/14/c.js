7194d3b159c29e759fa9243d09dee069ea9fc843
Serhii-Savchuk-Astacus <astacus.work@gmail.com>
2020-08-05 18:42:47 +0300

    precommit changed

--- src/main/resources/static/f/j2c/14/c.js
+++ src/main/resources/static/f/j2c/14/c.js
@@ -8,24 +8,19 @@ app.controller('AppCtrl', function ($scope, $http, $timeout) {
 	ctrl.selectItemId;
 	ctrl.selectedItemObject1;
 	ctrl.selectedItemObject2;
+	ctrl.newElementObject;
 })
 
-app.directive('workSpace', function () {
+app.directive('workConstruct', function () {
 	return {
 		replace: true,
 		restrict: 'AE',
 		link: function (scope, element, attr) {
 			element.on('click', function () {
 				ctrl.isShow = false;
-				ctrl.templateEditId = attr.workSpace;
+				ctrl.templateEditId = attr.workConstruct;
 				ctrl.selectedItemObject1 = ctrl.eMap[ctrl.templateEditId];
-				if (attr.workSpace >= 371833 && attr.workSpace <= 371847) {
-					ctrl.templateView = 'Construct';
-				} else if (attr.workSpace == 371832) {
-					ctrl.templateView = 'General';
-				} else {
 					ctrl.templateView = 'ConstructSettings';
-				}
 			})
 		}
 	}
@@ -50,4 +45,13 @@ app.directive('selectItem', function () {
 		}
 	}
 })
-
+app.directive('newElement',function(){
+	return{
+		restrict: 'A',
+		link: function(scope, element, attr){
+				element.on('click',function(){
+						
+				})
+		}
+	}
+})
