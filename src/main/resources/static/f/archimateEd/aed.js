1676c4060e673ca81e834794925a53000f73a128
romanm <roman.mishchenko@gmail.com>
2021-04-18 23:42:55 +0300

    archimateEl.initDeepElMap

--- src/main/resources/static/f/archimateEd/aed.js
+++ src/main/resources/static/f/archimateEd/aed.js
@@ -4,6 +4,7 @@ angular.element(() => angular.bootstrap(document, ['app']))
 const parser = new DOMParser()
 const conf = {}
 conf.openedFolderList = []
+conf.elMap = {}
 
 //app.factory("am2f", ArchiMateFileFactory)
 class ArchiMateFileFactory {
@@ -16,11 +17,25 @@ class ArchiMateFileFactory {
             conf.openedFolderList.push(x.id)
         }
         console.log(1, x.id)
+        conf.currentX = x
     }
 }
 app.factory("am2f", ArchiMateFileFactory)
 
 // app.controller("FirstController", FirstController)
+const initFirstElMap = (x) => {
+    conf.firstListIds.push(x.id)
+    conf.elMap[x.id] = x
+    initDeepElMap(x)
+}
+const initDeepElMap = (x) => {
+    if (x.children) angular.forEach(x.children, (x) => {
+        if (x.id) {
+            conf.elMap[x.id] = x
+            initDeepElMap(x)
+        }
+    })
+}
 class FirstController {
     am2f
     constructor($http, am2f) {
@@ -32,10 +47,11 @@ class FirstController {
                 const xmlDoc = parser.parseFromString(response.data, "text/xml")
                 ctrl.xmlDoc = xmlDoc
                 console.log(xmlDoc.firstChild.getAttribute('name'), xmlDoc.firstChild)
-                angular.forEach(xmlDoc.firstChild.children, (x) => {
-                    // console.log(x.localName, x.getAttribute('name'), x)
-                    console.log(x.localName, x.getAttribute('name'))
-                })
+                conf.firstListIds = []
+                angular.forEach(xmlDoc.firstChild.children, (x) => initFirstElMap(x))
+                let vf = conf.firstListIds.splice(conf.firstListIds.length - 1, 1)
+                conf.firstListIds.splice(0, 0, vf)
+                console.log(conf.firstListIds)
             })
     }
 }
