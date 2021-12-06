3c05acbe4d13a705cd1fd9b8079c54448502b3ce
AndreyRudik04 <andreyrudik04@gmail.com>
2021-12-06 16:30:44 +0200

    Kassa2   ver4

--- src/main/resources/static/f/kassa2r/i.js
+++ src/main/resources/static/f/kassa2r/i.js
@@ -21,53 +21,46 @@ class LRLController extends AbstractController {
 app.controller("LRLController", LRLController)
 
 let formData = {}
+formData.posting = {}
+formData.posting.LName = 'Iванов'
+formData.posting.myNumb = 1234
+formData.posting.firstName = 'ФОП Глобус'
+formData.posting.lastName = 'Сидоренко М.Н.'
+formData.posting.d1 = new Date('2021-01-01')
+formData.posting.d2 = new Date('2022-02-02')
+formData.posting.d3 = new Date()
+formData.posting.Nal = -1
+formData.posting.val = 'грн'
+formData.posting.KassOp = 'за товар'
+formData.posting.n1 = 'insert-kassa.html'
+formData.posting.n2 = 'table-kassa.html'
+formData.posting.Vrec = 0
+formData.posting.Vsum = 0
+formData.posting.PrRasx = 1
+
+conf.sqlKeyName = 'SelectKassa'
+console.log(formData.firstName)
+
 
 class TestControl {
     constructor($scope, $http) {
 
         this.formData = formData
 
-        this.firstName = 'ФОП Глобус';
-        this.lastName = 'Сидоренко М.Н.';
-
-        console.log(this.firstName)
-
-        $scope.d1 = new Date('2021-01-01')
-        $scope.d2 = new Date('2022-02-02')
-        $scope.d3 = new Date()
-
-        this.formData.Nal = -1
-        
-        this.formData.val = 'грн'
-
-        this.formData.KassOp = 'за товар'
-        
-        this.formData.LName = 'Иванов '
-
-        this.formData.myNumb = 123
-        
-        conf.sqlKeyName = 'SelectKassa'
-        $scope.n1 = 'insert-kassa.html'
-        $scope.n2 = 'table-kassa.html'
-        $scope.Vrec = 0
-        $scope.Vsum = 0
-        $scope.PrRasx = 1
-
         this.selectRow = (row) => {
             console.log(123, row)
             this.selectedRow = row
         }
 
-
-        $scope.AddKassa = () => {
+        this.AddKassa = () => {
             console.log("AddKassa", this.formData.Val)
-            sql = sql_app.AddKassa_VB.sql.replace(':pr', $scope.PrRasx)
-                .replace(':Ld1', "'" + $scope.d3.toISOString().split('T')[0] + "'")
-                .replace(':ssum',            formData.myNumb)
-                .replace(':KassOp', "'" +    formData.KassOp + "'")
-                .replace(':NameContr', "'" + formData.LName + "'")
-                .replace(':val', "'" + this.formData.Val + "'")
-                .replace(':nal',  this.formData.Nal )
+            sql = sql_app.AddKassa_VB.sql.replace(':pr', formData.posting.PrRasx)
+                .replace(':Ld1', "'" + formData.posting.d3.toISOString().split('T')[0] + "'")
+                .replace(':ssum', formData.posting.myNumb)
+                .replace(':KassOp', "'" + formData.posting.KassOp + "'")
+                .replace(':NameContr', "'" + formData.posting.LName + "'")
+                .replace(':val', "'" + formData.posting.Val + "'")
+                .replace(':nal', formData.posting.Nal)
 
             sql = sql + '; \n\ ' + makeSelect()
             console.log(sql)
@@ -82,7 +75,7 @@ class TestControl {
             console.log(12)
         }
 
-        $scope.DeleteKassa = (a) => {
+        this.DeleteKassa = (a) => {
             console.log("DeleteKassa", this.selectedRow.idnom)
             sql = sql_app.DeleteKassa.sql.replace(':LidNom', this.selectedRow.idnom)
             sql = sql + '; \n\ ' + makeSelect()
@@ -98,32 +91,41 @@ class TestControl {
         }
 
         const makeSelect = () => sql_app.SelectKassa.sql
-            .replace(':d1', "'" + $scope.d1.toISOString().split('T')[0] + "'")
-            .replace(':d2', "'" + $scope.d2.toISOString().split('T')[0] + "'")
-            .replace(':p', $scope.PrRasx)
-
-        const makeGroupSelect = () => sql_app.GroupKassa.sql
-            .replace(':d1', "'" + $scope.d1.toISOString().split('T')[0] + "'")
-            .replace(':d2', "'" + $scope.d2.toISOString().split('T')[0] + "'")
-            .replace(':p', $scope.PrRasx)
+            .replace(':d1', "'" + formData.posting.d1.toISOString().split('T')[0] + "'")
+            .replace(':d2', "'" + formData.posting.d2.toISOString().split('T')[0] + "'")
+            .replace(':p', formData.posting.PrRasx)
 
 
-        $scope.Perechet = (a) => {
-            console.log("b4 - group")
-            sql = makegroupSelect()
+       this.Perechet = () => {
+            console.log("Неполучаеться сбросить в переменные значения из запроса")
+
+            sql = sql_app.GroupKassa.sql
+                .replace(':d1', "'" + formData.posting.d1.toISOString().split('T')[0] + "'")
+                .replace(':d2', "'" + formData.posting.d2.toISOString().split('T')[0] + "'")
+                .replace(':P', formData.posting.PrRasx)
+            console.log(sql)
+
+
 
             $http.get('/r/url_sql_read_db1'
                 , { params: { sql: sql } }).then((responce) => {
                     $scope.data = responce.data
-                    $scope.Vrec = responce.data.list1.count
-                })
-            console.log(Vrec)
 
+
+                    // Тут не работает
+                    formData.posting.Vrec = responce.data.list.Count
+                    formData.posting.Vsum = responce.data.list.SumaProv
+                })
+                
+                // а здесь не видит this.formData.selectRow
+                console.log("GropKassa", this.formData.selectRow)
+
+            
+            
 
         }
 
-
-        $scope.Ok_button = (a) => {
+        this.Ok_button = (a) => {
             console.log("Ok_button")
             sql = makeSelect()
             $http.get('/r/url_sql_read_db1'
@@ -133,11 +135,7 @@ class TestControl {
             console.log(sql)
         }
 
-
-        let sql = sql_app.SelectKassa.sql.replace(':d1', "'" + $scope.d1.toISOString().split('T')[0] + "'").replace(':d2', "'" + $scope.d2.toISOString().split('T')[0] + "'").replace(':p', 1)
-
-         
-
+        let sql = sql_app.SelectKassa.sql.replace(':d1', "'" + formData.posting.d1.toISOString().split('T')[0] + "'").replace(':d2', "'" + formData.posting.d2.toISOString().split('T')[0] + "'").replace(':p', 1)
 
         console.log(sql)
 
