d7e9676458b998a498e49766d3ff864ea4da6a94
AndreyRudik04 <andreyrudik04@gmail.com>
2021-12-09 13:20:08 +0200

    kassa2 ver4

--- src/main/resources/static/f/kassa2r/config.sql.js
+++ src/main/resources/static/f/kassa2r/config.sql.js
@@ -3,38 +3,44 @@
 sql_app.SelectKassa = {
     name: 'Касса ',
     sql: 'SELECT idNom, DateProv, SumaProv, NameKassop, NameContr, NameVal, Nal_beznal  \n\
-    FROM kassa.finans \n\
+    FROM kassa.entry \n\
     WHERE DateProv >=:d1 AND DateProv<=:d2 AND Pr_rasx=:p ',
     sqlHtml: {
         dateprov: "{{v | date : 'medium'}}",
     },
 }
 
+sql_app.GroupKassa2 = {
+    name: 'Общая сума и кол-во проводок 2',
+    sql: 'SELECT COUNT(*) idNom, \'.     ∑∑\' DateProv, SUM(SumaProv) SumaProv  \n\
+    FROM  (:sql_app.SelectKassa ) x ',
+}
+
 sql_app.GroupKassa = {
     name: 'Общая сума и кол-во проводок ',
-    sql: 'SELECT COUNT(*) idNom, \'∑\' DateProv, SUM(SumaProv) SumaProv  \n\
-        FROM  kassa.finans \n\
+    sql: 'SELECT COUNT(*) idNom, \'.     ∑∑\' DateProv, SUM(SumaProv) SumaProv  \n\
+        FROM  kassa.entry \n\
         WHERE DateProv >=:d1 AND DateProv<=:d2 AND Pr_rasx=:p'  ,
 }
 
 sql_app.AddKassa_VB = {
     name: 'Добавление кассового ордера',
-    sql: 'INSERT INTO kassa.finans \n\
+    sql: 'INSERT INTO kassa.entry \n\
     (pr_rasx, idNom \n\
     , DateProv, SumaProv, NameKassOp, NameContr, nameval, nal_beznal) \n\
-    VALUES (:pr, (SELECT max(idnom)+1 FROM kassa.finans), \n\
+    VALUES (:pr, (SELECT max(idnom)+1 FROM kassa.entry), \n\
     :Ld1, :ssum, :KassOp, :NameContr, :val, :nal)' ,
 }
 
 
 sql_app.UpdateKassa = {
     name: 'Изменение номера сопутствующего документа',
-    sql: 'UPDATE kassa.finans SET idDoc=:LIdDoc WHERE idNom=:LidNom',
+    sql: 'UPDATE kassa.entry SET idDoc=:LIdDoc WHERE idNom=:LidNom',
 }
 
 sql_app.DeleteKassa = {
     name: 'Удаление проводки',
-    sql: 'DELETE FROM kassa.finans WHERE idNom=:LidNom',
+    sql: 'DELETE FROM kassa.entry WHERE idNom=:LidNom',
 }
 
 sql_app.SpContragent = {
@@ -55,4 +61,14 @@ sql_app.SpKassOp = {
 sql_app.SpValut = {
     name: 'Справочник валют',
     sql: 'SELECT IdNomVal, NameVal FROM kassa.SpVal ORDER BY NameVal',
+}
+
+sql_app.Seek_LName = {
+    name: 'Пошук по контрагенту',
+    sql: 'SELECT idNom, DateProv, SumaProv, NameKassop, NameContr, NameVal, Nal_beznal  \n\
+          FROM kassa.entry \n\
+          WHERE DateProv >=:d1 AND DateProv<=:d2 AND Pr_rasx=:p :seek ',
+    sqlHtml: {
+        dateprov: "{{v | date : 'medium'}}",
+    },
 }
