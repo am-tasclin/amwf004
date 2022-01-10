4296957218eb4feb603f109792d10eab6f65d92c
romanm <roman.mishchenko@gmail.com>
2022-01-11 00:10:31 +0200

    r2_value_22

--- src/main/resources/static/f/tese/02/i.js
+++ src/main/resources/static/f/tese/02/i.js
@@ -8,10 +8,12 @@ sql_app.SelectADN = {
     sql: 'SELECT d.*, s.value value_22, su.value value_u_22, o.sort \n\
     , srr.value rr_value_22 \n\
     , sr.value r_value_22, dr.doctype r_doctype \n\
+    , sr2.value r2_value_22 \n\
     FROM tese.doc d \n\
      LEFT JOIN sort o ON sort_id=d.doc_id \n\
      LEFT JOIN string_u su ON su.string_u_id=d.doc_id \n\
      LEFT JOIN string sr ON sr.string_id=d.reference \n\
+     LEFT JOIN string sr2 ON sr2.string_id=d.reference2 \n\
      LEFT JOIN doc dr ON dr.doc_id=d.reference \n\
      LEFT JOIN string srr ON srr.string_id=dr.reference \n\
      LEFT JOIN string s ON s.string_id=d.doc_id',
@@ -70,6 +72,8 @@ class InitPageController extends AbstractController {
     initSession = () => JSON.stringify(singlePage.session)
 
     initRL = () => {
+        console.log(this.dataFactory
+            .buildSqlWithKeyValue('SelectADN', 'doc_id', singlePage.session.tree.l.id))
         this.dataFactory.getReadADN(singlePage.session.tree.l.id)
         if (singlePage.session.tree.r.id)
             this.dataFactory.getReadADN(singlePage.session.tree.r.id)
@@ -114,7 +118,7 @@ class InitChildrenController extends InitPageController {
         super(dataFactory)
         if (!singlePage.session.tree.l.id)
             singlePage.session.tree.l.id = singlePage.UrlMap()['children']
-        console.log( singlePage.UrlMap()['children'])
+        console.log(singlePage.UrlMap()['children'])
         // console.log(singlePage.session)
 
         this.dataFactory.getReadADN_children(singlePage.UrlMap()['children'])
